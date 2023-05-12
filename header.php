e2365e27a872fae8544f0dcf5eb6490cd3e4031f
Patrik Vaďura <patrikvadura@icloud.com>
2023-05-12 16:06:37 +0200

    add: GA

--- header.php
+++ header.php
@@ -13,11 +13,22 @@ namespace acdfevelop;
 <html <?php language_attributes(); ?>>
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0CRRFVR5H5"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-0CRRFVR5H5');
+    </script>
+
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?php bloginfo('description'); ?>">
 
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
     <?php wp_head(); ?>
 </head>
